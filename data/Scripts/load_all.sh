ad3b86702bdac9e6aa8894fa8e70a4de09e1399e
anombela <a.nombela93@hotmail.com>
2018-04-29 19:00:41 +0200

    editado .sh

--- data/Scripts/load_all.sh
+++ data/Scripts/load_all.sh
@@ -2,6 +2,10 @@
 # ./load_all.sh <path_voltdb> <path_data> <limitrows> <csv_tripdata_colour2016> <csv_tripdata_colour2017> <csv_taxi_zones>
 # ./load_all.sh /home/alex/Escritorio/TFG/voltdb/voltdb /home/alex/Escritorio/TFG-taxis/data 10000 yellow_tripdata_2016-06.csv yellow_tripdata_2017-06.csv taxi-zone-lookup-with-ntacode.csv
 
+
+#ejemplo de ejecución de alfonso
+#./load_all.sh /home/anombela/Escritorio/TFG/voltdb/voltdb/ /home/anombela/Escritorio/TFG-taxis/data 10000 yellow_tripdata_2016-06.csv yellow_tripdata_2017-06.csv taxi-zone-lookup-with-ntacode.csv
+
 javac -cp "$CLASSPATH:$1/*" $2/files/*.java
 cd $2/files
 jar cvf $2/files/storedprocs.jar *.class
