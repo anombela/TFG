96f49a43d7afafb51e34938ddd92a82a78e3170e
A-malagon <alejanm09@gmail.com>
2018-05-02 11:06:24 +0200

    recortado y subidos datos utiles y tabla central_park_weather

--- data/Scripts/load_all.sh
+++ data/Scripts/load_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# ./load_all.sh <path_voltdb> <path_data> <limitrows> <csv_tripdata_colour2016> <csv_tripdata_colour2017> <csv_taxi_zones>
-# ./load_all.sh /home/alex/Escritorio/TFG/voltdb/voltdb /home/alex/Escritorio/TFG-taxis/data 10000 yellow_tripdata_2016-06.csv yellow_tripdata_2017-06.csv taxi-zone-lookup-with-ntacode.csv
+# ./load_all.sh <path_voltdb> <path_data> <limitrows> <csv_tripdata_colour2016> <csv_tripdata_colour2017> <csv_taxi_zones> <csv_weather>
+# ./load_all.sh /home/alex/Escritorio/TFG/voltdb/voltdb /home/alex/Escritorio/TFG-taxis/data 10000 yellow_tripdata_2016-06.csv yellow_tripdata_2017-06.csv taxi-zone-lookup-with-ntacode.csv central_park_weather.csv
 
 
 #ejemplo de ejecución de alfonso
-#./load_all.sh /home/anombela/Escritorio/TFG/voltdb/voltdb/ /home/anombela/Escritorio/TFG-taxis/data 10000 yellow_tripdata_2016-06.csv yellow_tripdata_2017-06.csv taxi-zone-lookup-with-ntacode.csv
+#./load_all.sh /home/anombela/Escritorio/TFG/voltdb/voltdb/ /home/anombela/Escritorio/TFG-taxis/data 10000 yellow_tripdata_2016-06.csv yellow_tripdata_2017-06.csv taxi-zone-lookup-with-ntacode.csv central_park_weather.csv
 
 javac -cp "$CLASSPATH:$1/*" $2/files/*.java
 cd $2/files
@@ -12,6 +12,8 @@ jar cvf $2/files/storedprocs.jar *.class
 echo -e "load classes $2/files/storedprocs.jar" | sqlcmd
 echo -e "FILE $2/SQLs/taxis2016.sql" | sqlcmd
 echo -e "FILE $2/SQLs/taxis2017.sql" | sqlcmd
+echo -e "FILE $2/SQLs/commun.sql" | sqlcmd
 csvloader --skip 1 --file $2/../CSVs/$4 taxis2016 --limitrows $3
 csvloader --skip 1 --file $2/../CSVs/$5 taxis2017 --limitrows $3
 csvloader --skip 1 --file $2/../CSVs/$6 location --limitrows $3
+csvloader --skip 1 --file $2/../CSVs/$7 central_park_weather --limitrows $3
