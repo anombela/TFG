fd9d98e65ddfcdab432d16fe2176985a41bfa7fc
A-malagon <alejanm09@gmail.com>
2018-05-02 13:01:38 +0200

    prueba tú

--- data/SQLs/commun.sql
+++ data/SQLs/commun.sql
@@ -9,3 +9,18 @@ CREATE TABLE central_park_weather (
    tmax INTEGER,
    tmin INTEGER,
 );
+
+CREATE PROCEDURE total_voided_trips
+
+DECLARE @table VARCHAR(64)  
+SET @table = ?
+IF @table == "taxis2017"
+   SELECT count(payment_type) AS total_voided_trips
+   FROM taxis2017
+   WHERE payment_type = 6;
+
+--CREATE PROCEDURE total_voided_trips
+--  AS 
+--    SELECT count(payment_type) AS total_voided_trips
+--    FROM ?
+--    WHERE payment_type = 6;
